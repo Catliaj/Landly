0e1f95eb5b0294a48fafc71fd87dfcb066e6b2eb
Catliaj <ajcatli26@gmail.com>
2026-03-12 01:37:53 +0800

    profie picture path update

--- app/Controllers/Auth/SignUpController.php
+++ app/Controllers/Auth/SignUpController.php
@@ -13,18 +13,29 @@ class SignUpController extends BaseController
     {
         // Logic for buyer sign-up
         $buyerData = $this->request->getPost();
-        
-       
-       
-        
+
+        $buyerData['roles'] = 'buyer';
+
         $userModel = new UserModel();
         //check if email already exists
         $existingEmail = $userModel->checkEmailExists($buyerData['email']);
-        if ($existingEmail && $buyerData['roles'] === 'buyer') {
+        if ($existingEmail) {
             return $this->response->setJSON(['message' => 'Email already exists'])->setStatusCode(ResponseInterface::HTTP_CONFLICT);
         }
-        
+
+        //create a folder destination for profile picture if folder exist  do not create folder else if folder does not exist create folder name profile_pictures
+        if (!is_dir(WRITEPATH . 'uploads/profile_pictures')) {
+            mkdir(WRITEPATH . 'uploads/profile_pictures', 0755, true);
+        }
+
+        //store profile picture in the folder destination and get the stored path
+        $file = $this->request->getFile('profile_image');
+        if ($file && $file->isValid() && !$file->hasMoved()) {
+            $buyerData['profile_picture'] = $file->store('profile_pictures');
+        }
+
         $userModel->createUser($buyerData);
+
         return $this->response->setJSON(['message' => 'Buyer sign-up successful'])->setStatusCode(ResponseInterface::HTTP_OK);
        
     }
